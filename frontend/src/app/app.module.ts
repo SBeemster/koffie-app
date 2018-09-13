9ae91cab086229d824404f218fc6c2a5e9a547c9
sbeemster <sjoerd.beemster@gmail.com>
2018-09-13 20:08:00 +0200

    Toegevoegd: basisstructuur -> header / (placeholder) content / footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PlaceholderContentComponent } from './placeholder-content/placeholder-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PlaceholderContentComponent
   ],
   imports: [
     BrowserModule
---
+++ frontend/src/app/placeholder-content/placeholder-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-placeholder-content',
+  templateUrl: './placeholder-content.component.html',
+  styleUrls: ['./placeholder-content.component.scss']
+})
+export class PlaceholderContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
