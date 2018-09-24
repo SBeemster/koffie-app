f0be88e757ff505c98e1f770c37e086fa2f51d8f
jorisvdinther <j.vandinther@kw1c.nl>
2018-09-24 19:54:52 +0200

    formsmodule toegevoegd aan user module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
@@ -11,8 +10,7 @@ import { AppRoutingModule } from './/app-routing.module';
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule,
-        FormsModule
+        AppRoutingModule
     ],
     bootstrap: [AppComponent]
 })
