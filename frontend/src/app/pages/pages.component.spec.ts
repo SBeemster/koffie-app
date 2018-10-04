41b9d1e0b49f414dcdc1a254cc8b4cb963f9ee52
MarcovanDoren <36743482+MarcovanDoren@users.noreply.github.com>
2018-10-04 12:35:46 +0200

    SharedModule toegevoegd in pages.component.spec.ts

--- frontend/src/app/pages/pages.component.spec.ts
+++ frontend/src/app/pages/pages.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { PagesComponent } from "./pages.component";
+import { SharedModule } from "../shared/shared.module";
 
 describe("PagesComponent", () => {
   let component: PagesComponent;
@@ -8,7 +9,7 @@ describe("PagesComponent", () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, SharedModule],
       declarations: [PagesComponent]
     }).compileComponents();
   }));
