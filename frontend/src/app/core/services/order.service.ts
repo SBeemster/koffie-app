bda57417c48f4a27906ef858d7387b565591b0ae
MarcovanDoren <36743482+MarcovanDoren@users.noreply.github.com>
2018-10-11 17:47:33 +0200

    Suiker en melk toegevoegd aan de OrderLine en orderservice

--- frontend/src/app/core/services/order.service.ts
+++ frontend/src/app/core/services/order.service.ts
@@ -1,48 +1,66 @@
-import { Injectable } from '@angular/core';
-import { OrderLine } from '../classes/orderLine';
-
+import { Injectable } from "@angular/core";
+import { OrderLine } from "../classes/orderLine";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class OrderService {
   orders = [];
   getOrders(): Array<OrderLine> {
     return this.orders;
   }
-  gaHalen(): void{
-    for(let s of this.orders){
-      if(!s.verwerkt) {
+  gaHalen(): void {
+    for (let s of this.orders) {
+      if (!s.verwerkt) {
         s.verwerkt = true;
         s.halen = true;
       }
     }
   }
-  placeOrder(product: string, aantal: number, verbruiker: string): void {
+  placeOrder(
+    product: string,
+    aantal: number,
+    verbruiker: string,
+    melk: number,
+    suiker: number
+  ): void {
     for (const s of this.orders) {
-      if (s.name === product && s.verbruiker === verbruiker && s.verwerkt === false) {
+      if (
+        s.name === product &&
+        s.verbruiker === verbruiker &&
+        s.verwerkt === false
+      ) {
         s.aantal++;
         return;
       }
     }
-    const newProduct = new OrderLine(product ? product : 'Koffie', aantal ? aantal : 1, verbruiker ? verbruiker : '');
+    const newProduct = new OrderLine(
+      product ? product : "Koffie",
+      aantal ? aantal : 1,
+      verbruiker ? verbruiker : "",
+      melk ? melk : 0,
+      suiker ? suiker : 0
+    );
     this.orders.push(newProduct);
   }
   deleteFromOrder(order: OrderLine) {
     this.orders.splice(this.orders.indexOf(order), 1);
   }
-  setHalenFalse():void{
-    for(let s of this.orders) {
-      s.halen=false;
+  setHalenFalse(): void {
+    for (let s of this.orders) {
+      s.halen = false;
     }
   }
   clearCart(verbruiker: string): void {
-  for (let i=0; i < this.orders.length; i++) {
-    if(this.orders[i].verwerkt === false && this.orders[i].verbruiker === verbruiker){
-      this.orders.splice(i, 1);
-      i--;
+    for (let i = 0; i < this.orders.length; i++) {
+      if (
+        this.orders[i].verwerkt === false &&
+        this.orders[i].verbruiker === verbruiker
+      ) {
+        this.orders.splice(i, 1);
+        i--;
+      }
     }
   }
-  }
-  constructor() { }
+  constructor() {}
 }
