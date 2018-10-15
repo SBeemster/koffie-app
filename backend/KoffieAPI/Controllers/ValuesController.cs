8e7c36502f07c5b7193fda71988d950d5e695ce6
Dinther Joris van <j.vandinther@kw1c.nl>
2018-10-15 14:04:41 +0200

    dependicie system.sqlclient verwijderd

--- backend/KoffieAPI/Controllers/ValuesController.cs
+++ backend/KoffieAPI/Controllers/ValuesController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using System.Data.SqlClient;
 using System.Data;
 using KoffieAPI.Models;
 
