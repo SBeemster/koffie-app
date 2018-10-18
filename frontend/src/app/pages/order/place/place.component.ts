e0a896829d76f4785e3cfbf863fb5c30f58151b7
MarcovanDoren <36743482+MarcovanDoren@users.noreply.github.com>
2018-10-18 15:29:10 +0200

    Styling aangepast en routerlink naar choice component

--- frontend/src/app/pages/order/place/place.component.ts
+++ frontend/src/app/pages/order/place/place.component.ts
@@ -1,26 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { AvailableCoffeeService } from '../../../core/services/available-coffee.service';
-import { OrderService } from '../../../core/services/order.service';
+import { Component, OnInit } from "@angular/core";
+import { AvailableCoffeeService } from "../../../core/services/available-coffee.service";
+import { OrderService } from "../../../core/services/order.service";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-place',
-  templateUrl: './place.component.html',
-  styleUrls: ['./place.component.scss']
+  selector: "app-place",
+  templateUrl: "./place.component.html",
+  styleUrls: ["./place.component.scss"]
 })
 export class PlaceComponent implements OnInit {
-
-  orders=this.OrderService.orders;
+  orders = this.OrderService.orders;
   availableCoffees;
-  constructor(private availableCoffeeService: AvailableCoffeeService, private OrderService: OrderService) { }
+  constructor(
+    private availableCoffeeService: AvailableCoffeeService,
+    private OrderService: OrderService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.availableCoffees = this.availableCoffeeService.getCoffee();
   }
 
-  addToOrder=this.OrderService.placeOrder;
+  addToOrder = this.OrderService.placeOrder;
 
-  deleteFromOrder=this.OrderService.deleteFromOrder;
-   
-  clearCart=this.OrderService.clearCart;
-  
+  deleteFromOrder = this.OrderService.deleteFromOrder;
+
+  clearCart = this.OrderService.clearCart;
+
+  routeToChoice() {
+    this.router.navigate(["../choice"], { relativeTo: this.activatedRoute });
+  }
 }
