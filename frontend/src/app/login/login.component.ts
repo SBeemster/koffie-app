588c738f8d84ddf73dd60e0e4ad4b0293e14f1b2
sbeemster <sjoerd.beemster@gmail.com>
2018-10-21 13:36:56 +0200

    Aangepast. Loginpagina nu werkende post voor username en password. Tijdelijke HTTP service in module zelf.

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+    response: object = { "response": "no response yet..." };
+    passType: string = "password"
 
-  ngOnInit() {
-  }
+    username: string = "jaap";
+    password: string = "password";
 
+    constructor(private http: HttpClient) { }
+
+    togglePassword(): void {
+        if (this.passType === "password") {
+            this.passType = "text"
+        } else {
+            this.passType = "password"
+        }
+    }
+
+    login(): void {
+        let requestUrl = "https://localhost:44399/api/login";
+
+        let requestBody = {
+            "UserName": this.username,
+            "Password": this.password
+        }
+
+        let httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json'
+            })
+        };
+
+        this.http.post(requestUrl, requestBody, httpOptions)
+            .subscribe(
+                res => { this.response = res; this.setToken(res); }, //success
+                res => { this.response = res; }, //error
+            )
+    }
+
+    setToken(token: object) {
+        localStorage.setItem("authToken", token["token"]);
+        console.log(localStorage.getItem("authToken"));
+    }
+
+    removeToken(): void {
+        localStorage.removeItem("authToken");
+    }
+
+    getRequest(): void {
+        let requestUrl = "https://localhost:44399/api/users";
+
+        let httpOptions = {
+            headers: new HttpHeaders({
+                'Authorization': `Bearer ${localStorage.getItem("authToken")}`
+            })
+        };
+
+        this.http.get(requestUrl, httpOptions)
+            .subscribe(
+                res => { this.response = res; }, //success
+                res => { this.response = res; }, //error
+            )
+    }
 }
--- frontend/src/app/login/login.module.ts
+++ frontend/src/app/login/login.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; //temp
 
 import { LoginRoutingModule, routedComponents } from './/login-routing.module';
 
 @NgModule({
     imports: [
         CommonModule,
-        LoginRoutingModule
+        LoginRoutingModule,
+        FormsModule,
+        HttpClientModule //temp
     ],
     declarations: [...routedComponents]
 })
