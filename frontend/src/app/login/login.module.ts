371a315f0f6d31ae65facbf6cd9018969f4b98d6
sbeemster <sjoerd.beemster@gmail.com>
2018-10-22 19:01:47 +0200

    Toegevoegd: @auth0/angular-jwt, geimplementeerd in auth.service.js

--- frontend/src/app/login/login.module.ts
+++ frontend/src/app/login/login.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'; //temp
 
 import { LoginRoutingModule, routedComponents } from './/login-routing.module';
 
@@ -9,8 +8,7 @@ import { LoginRoutingModule, routedComponents } from './/login-routing.module';
     imports: [
         CommonModule,
         LoginRoutingModule,
-        FormsModule,
-        HttpClientModule //temp
+        FormsModule
     ],
     declarations: [...routedComponents]
 })
