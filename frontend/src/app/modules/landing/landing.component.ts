51fa051abdc7acb9486fb41e5d11b32473b263b6
Sjoerd Beemster <Sjoerd.Beemster@boschrexroth.nl>
2018-11-22 16:28:39 +0100

    Gefixt, redirect naar dashboard als gebruiker reeds is ingelogd

--- frontend/src/app/modules/landing/landing.component.ts
+++ frontend/src/app/modules/landing/landing.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "src/app/core/services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-landing',
-  templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+    selector: 'app-landing',
+    templateUrl: './landing.component.html',
+    styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        private auth: AuthService,
+        private router: Router
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    toLogin() {
+        if (this.auth.isLoggedIn()) {
+            this.router.navigateByUrl('/dashboard');
+        } else {
+            this.router.navigateByUrl('/login');
+        }
+    }
 
 }
