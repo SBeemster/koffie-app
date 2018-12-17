71a3961acdbdd625831a7cb453daab79d837d1f5
sbeemster <sjoerd.beemster@gmail.com>
2018-12-17 17:10:06 +0100

    Gesplitst: header in componenten, mogelijk fix voor niet aanpassen username na uitlog

---
+++ frontend/src/app/shared/header/group/group.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { GroupService } from 'src/app/core/services/group.service';
+import { Group } from 'src/app/core/classes/group';
+import { ApiService } from 'src/app/core/services/api.service';
+
+@Component({
+    selector: 'app-header-group',
+    templateUrl: './group.component.html',
+    styleUrls: ['./group.component.scss']
+})
+export class GroupComponent implements OnInit {
+
+    memberGroup: Group = {
+        groupId: "",
+        groupName: ""
+    }
+
+    groupFound = false;
+
+    constructor(
+        private api: ApiService,
+        private groupService: GroupService
+    ) { }
+
+    ngOnInit() {
+        this.groupService.getMemberGroup().subscribe(
+            response => {
+                if (response) {
+                    this.memberGroup = response;
+                    this.groupFound = true;
+                }
+            }
+        )
+    }
+
+    awaitingResponse(): boolean {
+        return this.api.awaitingResponse;
+    }
+}
